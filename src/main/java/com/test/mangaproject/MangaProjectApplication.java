7c5190550ee4f3398c6914cd2d23c290bbffc4b8
Rui <tostapedro@live.com.pt>
2020-05-28 18:43:02 +0100

    First commit

---
+++ src/main/java/com/test/mangaproject/MangaProjectApplication.java
@@ -0,0 +1,14 @@
+package com.test.mangaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangaProjectApplication.class, args);
+
+    }
+
+}
---
+++ src/main/java/com/test/mangaproject/TestController.java
@@ -0,0 +1,16 @@
+package com.test.mangaproject;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+
+    @GetMapping("/")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+}
